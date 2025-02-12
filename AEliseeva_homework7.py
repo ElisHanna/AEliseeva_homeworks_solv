37bb54890bb1060154b115fc9edf8b19772d8bdd
AnnaElis <kunicasbumerangom@gmail.com>
2025-02-12 23:54:05 +0300

    double spaces are deleted

--- AEliseeva_homework7.py
+++ AEliseeva_homework7.py
@@ -55,9 +55,9 @@ if hour > 23 or\
 else:
     if hour == 0:
         print(f'12:{minute} a.m.')
-    elif  0 < hour < 10:
+    elif 0 < hour < 10:
         print(f'{hour}:{minute} a.m')
-    elif  10 <= hour < 12:
+    elif 10 <= hour < 12:
         print(f'{time} a.m.')
     elif hour > 12:
         print(f'{hour - 12}:{minute} p.m.')
