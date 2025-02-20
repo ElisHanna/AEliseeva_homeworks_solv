05690ff484b9f1c2563ec579e32747c74a39284b
AnnaElis <kunicasbumerangom@gmail.com>
2025-02-20 12:39:56 +0300

    code is simplified

--- AEliseeva_homework9.py
+++ AEliseeva_homework9.py
@@ -1,12 +1,6 @@
 import os
-from string import ascii_letters, punctuation
 
 os.system('cls')
-
-cyrillic_lower = [(lambda c: chr(c))(i) for i in range(1072, 1104)]
-cyrillic_upper = [(lambda c: chr(c))(i) for i in range(1040, 1072)]
-cyrillic_ansi = cyrillic_lower + cyrillic_upper
-cyrillic_ansi.extend(['Ё', 'ё'])
 
 
 def rising_row(*args):
@@ -15,7 +9,7 @@ def rising_row(*args):
         return 'Please, give me some arguments'
 
     for items in args:
-        if type(items) is list:
+        if isinstance(items, list):
             array = list.copy(items)
         else:
             array = [items for items in args]
@@ -24,7 +18,7 @@ def rising_row(*args):
         return "Array for check can't be empty"
 
     for argument in array:
-        if type(argument) is not int:
+        if not isinstance(argument, int):
             return "Sorry, I can compare only integers"
 
     new_array = [array[0]]
@@ -62,22 +56,13 @@ def luhn(number):
 
     if len(number) == 0:
         return 'Give me your number'
-    elif len(number) < 13:
-        return 'Your number is too short, I do not trust you -_-'
-    elif len(number) > 19:
-        return 'Your number is too long, I do not trust you -_-'
+    elif len(number) < 10 or\
+         len(number) > 19:
+         return 'Wrong lehgth, I do not trust you -_-'
 
-    for sign in punctuation:
-        if sign in number:
-            return 'Card number cannot contain punctuation'
-
-    for bukv in cyrillic_ansi:
-        if bukv in number:
-            return 'Card number cannot contain letters'
-
-    for lett in ascii_letters:
-        if lett in number:
-            return 'Card number cannot contain letters'
+    for compon in number:
+        if not compon.isdigit():
+            return 'Number contains wrong symbols'
 
     number = [int(symb) for symb in number]
     number.reverse()
