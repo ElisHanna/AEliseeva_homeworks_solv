6bd1a4dcdf569bec04526ae13f16f4e81a2d5a59
AnnaElis <kunicasbumerangom@gmail.com>
2025-03-10 22:03:03 +0300

    Homework #13 main done

--- AEliseeva_homework12_bank.py
+++ AEliseeva_homework12_bank.py
@@ -7,6 +7,31 @@ class BankError(Exception):
     pass
 
 
+class Person:
+
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+
+class CurrencyConverter:
+
+    def __init__(self, rates):
+        self.rates = rates
+
+    def exchange_currency(self, from_currency, amount, to_currency = 'BYN'):
+        if from_currency not in self.rates:
+            raise BankError(f'There is no such currency {from_currency}')
+
+        if to_currency not in self.rates:
+            raise BankError(f'There is no such currency {to_currency}')
+
+        key = self.rates[from_currency]
+        x = key[to_currency]
+        result = round((amount * x), 2)
+        return result, to_currency
+
+
 class Bank:
 
     def __init__(self):
@@ -16,6 +41,7 @@ class Bank:
         self.years = None
         self.interest = None
         self.clients = []
+        self.converter = converter
 
     def register_client(self, client_id, name):
         if client_id not in self.clients:
@@ -24,6 +50,12 @@ class Bank:
             self.clients.append(client_id)
         else:
             raise BankError('Client is already registered')
+
+    def convert_currency(self, client_id, from_currency, amount, to_currency='BYN'):
+        if client_id in self.clients:
+            return self.converter.exchange_currency(from_currency, amount, to_currency)
+        else:
+            raise BankError('Client is not registered')
 
     def open_deposit_account(self, client_id, start_balance, years):
         if client_id in self.clients:
@@ -53,12 +85,29 @@ class Bank:
             self.interest = None
 
 
-# client_id = '00001'
+rates = {
+            'BYN':{'BYN':1.0000, 'USD':0.3149, 'EUR':0.2896},
+            'EUR':{'BYN':3.4530, 'USD':1.0872, 'EUR':1.0000},
+            'USD':{'BYN':3.1760, 'USD':1.0000, 'EUR':0.9198}
+    }
+
+converter = CurrencyConverter(rates = rates)
+
+vasya = Person('USD', 10)
+petya = Person('EUR', 5)
+
+print(converter.exchange_currency(vasya.currency, vasya.amount))
+print(converter.exchange_currency(petya.currency, petya.amount))
+print(converter.exchange_currency(vasya.currency, vasya.amount, 'EUR'))
+print(converter.exchange_currency(petya.currency, petya.amount, 'USD'))
 
 bank = Bank()
 
 bank.register_client(client_id='00001', name='Igor')
+
 bank.open_deposit_account(client_id='00001', start_balance=1000, years=1)
-bank.calc_interest_rate(client_id='00001')
-bank.close_deposit(client_id='00001')
-print(bank.clients)
+print(bank.convert_currency('00001', 'USD', 10, 'EUR'))
+print(bank.convert_currency('00001', 'USD', 10,))
+# bank.calc_interest_rate(client_id='00001')
+# bank.close_deposit(client_id='00001')
+# print(bank.clients)
---
+++ AEliseeva_homework13_cards.py
@@ -0,0 +1,46 @@
+import os
+
+os.system('cls')
+
+
+class Card:
+
+    number_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    suit_list = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+
+    def __init__(self):
+        self.number_list = Card.number_list
+        self.suit_list = Card.suit_list
+
+
+class CardsDeck:
+
+    def __init__(self):
+        self.deck = []
+
+    def shuffle(self):
+        self.deck = ['Night Joker', 'Day Joker']
+        for suit in Card.suit_list:
+            for number in Card.number_list:
+                self.deck.append(suit + ' ' + number)
+        self.deck = set(self.deck)
+        return self.deck
+
+    def get(self, card_number):
+        self.deck = list(self.deck)
+        lenght = len(self.deck)
+        if card_number not in range(lenght):
+            print("Don't fool around please!")
+        else:
+            return self.deck[card_number - 1]
+
+
+deck = CardsDeck()
+deck.shuffle()
+card_number = int(input('Choose the card from the 54-card deck: '))
+card = deck.get(card_number)
+print(f'Your card is: {card}')
+
+card_number = int(input('Choose the card from the 54-card deck: '))
+card = deck.get(card_number)
+print(f'Your card is: {card}')
