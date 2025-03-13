ebbb58996935af13e04e07d595c9154139198a4d
AnnaElis <kunicasbumerangom@gmail.com>
2025-03-13 17:42:09 +0300

    xml updated

--- homework14/xmlfile.py
+++ homework14/xmlfile.py
@@ -7,11 +7,15 @@ os.system('cls')
 tree = ET.parse('goods.xml')
 root = tree.getroot()
 
-sum = 0
+sum = 0.0
 for prod in root.findall('product'):
-    price = prod.find('price').text if prod.find('price') is not None else '0'
-    quantity = prod.find('qt').text if prod.find('qt') is not None else '0'
-    money = int(price)
-    number = int(quantity)
+    price = prod.find('price')
+    quantity = prod.find('qt')
+
+    text_price = price.text if prod.find('qt') is not None else '0'
+    text_quantity = quantity.text if prod.find('qt') is not None else '0'
+
+    money = float(text_price) if text_price is not None else False
+    number = int(text_quantity) if text_quantity is not None else False
     sum += money * number
 print(f'Summary price is {sum}')
