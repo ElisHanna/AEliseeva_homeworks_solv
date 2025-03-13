f0ddad6c376c3db228c26ea47031bc1296f21a13
AnnaElis <kunicasbumerangom@gmail.com>
2025-03-13 17:48:12 +0300

    xmlfile fixed

--- homework14/xmlfile.py
+++ homework14/xmlfile.py
@@ -12,8 +12,8 @@ for prod in root.findall('product'):
     price = prod.find('price')
     quantity = prod.find('qt')
 
-    text_price = price.text if prod.find('qt') is not None else '0'
-    text_quantity = quantity.text if prod.find('qt') is not None else '0'
+    text_price = price.text if price is not None else False
+    text_quantity = quantity.text if quantity is not None else False
 
     money = float(text_price) if text_price is not None else False
     number = int(text_quantity) if text_quantity is not None else False
