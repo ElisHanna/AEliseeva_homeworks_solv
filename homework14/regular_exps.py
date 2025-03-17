aa7d8cb267f978ec987cb2dceede132beeede8bf
AnnaElis <kunicasbumerangom@gmail.com>
2025-03-17 23:39:06 +0300

    blank line added

--- homework14/regular_exps.py
+++ homework14/regular_exps.py
@@ -6,6 +6,7 @@ os.system('cls')
 with open('regexp.txt') as testfile:
     text = testfile.readlines()
 
+
 def looking_for_dates(doc):
     pattern = r"(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})"
     res = []
