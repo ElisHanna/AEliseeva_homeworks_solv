dbff540daf438fc1a10f528b1ec1da3cf3647ac1
AnnaElis <kunicasbumerangom@gmail.com>
2025-04-10 16:18:59 +0300

    homework21 done

---
+++ homework21/apps/bank.py
@@ -0,0 +1,109 @@
+import os
+import logging
+
+os.system('cls')
+
+logger = logging.getLogger()
+file = logging.FileHandler('test_bank_pytest.log')
+file.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+logger.addHandler(file)
+logger.setLevel(logging.DEBUG)
+
+
+class BankError(Exception):
+    pass
+
+
+class Person:
+
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+
+class CurrencyConverter:
+
+    def __init__(self, rates):
+        self.rates = rates
+
+    def exchange_currency(self, from_currency, amount, to_currency='BYN'):
+        if from_currency not in self.rates:
+            logger.error('BankError raised')
+            raise BankError(f'There is no such currency {from_currency}')
+
+        if to_currency not in self.rates:
+            logger.error('BankError raised')
+            raise BankError(f'There is no such currency {to_currency}')
+
+        key = self.rates[from_currency]
+        x = key[to_currency]
+        result = round((amount * x), 2)
+        return result, to_currency
+
+
+class Bank:
+
+    def __init__(self):
+        self.res = 0
+        self.client_id = None
+        self.name = None
+        self.start_balance = None
+        self.years = None
+        self.interest = None
+        self.clients = []
+        self.converter = CurrencyConverter(rates=currency_rates)
+
+    def register_client(self, client_id, name):
+        if client_id not in self.clients:
+            self.client_id = client_id
+            self.name = name
+            self.clients.append(client_id)
+        else:
+            logger.error('BankError raised')
+            raise BankError('Client is already registered')
+
+    def convert_currency(self, client_id, from_currency, amount, to_currency='BYN'):
+        if client_id in self.clients:
+            return self.converter.exchange_currency(from_currency, amount, to_currency)
+        else:
+            logger.error('BankError raised')
+            raise BankError('Client is not registered')
+
+    def open_deposit_account(self, client_id, start_balance, years):
+        if client_id in self.clients:
+            self.start_balance = start_balance
+            self.years = years
+        else:
+            logger.error('BankError raised')
+            raise BankError('Client is not registered!')
+
+    def calc_interest_rate(self, client_id):
+        if client_id in self.clients:
+            month = self.years*12
+            counter = 0
+            interest = self.start_balance
+            while counter < month:
+                interest += interest*(10/12)/100
+                counter += 1
+                self.interest = interest
+            self.interest = round(interest, 2)
+            interest = round(interest, 2)
+            return interest
+        else:
+            logger.error('BankError raised')
+            raise BankError('Client is not registered!')
+
+    def close_deposit(self, client_id):
+        self.res = self.interest
+        if client_id in self.clients:
+            self.start_balance = None
+            self.years = None
+            self.interest = None
+            return f'Client got {self.res} money. Deposit is closed'
+
+
+currency_rates = {
+            'BYN': {'BYN': 1.0000, 'USD': 0.3149, 'EUR': 0.2896},
+            'EUR': {'BYN': 3.4530, 'USD': 1.0872, 'EUR': 1.0000},
+            'USD': {'BYN': 3.1760, 'USD': 1.0000, 'EUR': 0.9198}
+    }
---
+++ homework21/tests/test_library.py
@@ -0,0 +1,113 @@
+import os
+import pytest
+import logging
+from apps.library import Book, Reader
+
+os.system('cls')
+
+logger = logging.getLogger()
+file = logging.FileHandler('test_lib_pytest.log')
+file.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+logger.addHandler(file)
+logger.setLevel(logging.DEBUG)
+
+
+@pytest.fixture
+def book_one():
+    return Book('Her', 'J.Doe', '150', '0001')
+
+
+@pytest.fixture
+def book_two():
+    return Book('Him', 'J.Doe', '100', '0002')
+
+
+@pytest.fixture
+def reader_one():
+    return Reader('Geralt')
+
+
+@pytest.fixture
+def reader_two():
+    return Reader('Lyutik')
+
+
+def test_reserve(book_one):
+    assert book_one.reserve() == f'{book_one.title} by {book_one.author} is successfully booked'
+    assert book_one.status == 'booked'
+    logger.info('Test test_reserve passed')
+
+
+def test_cancel_reserve(book_one):
+    book_one.status = 'booked'
+    assert book_one.cancel_reserve() == 'Your booking was successfully cancelled'
+    assert book_one.status == 'available'
+    assert book_one.cancel_reserve() == None
+    logger.info('Test test_cancel_reserve passed')
+
+
+def test_get_book(book_one, book_two):
+    book_one.status = 'booked'
+    book_two.status = 'available'
+    assert book_one.get_book() == 'User got the book'
+    assert book_two.get_book() == 'User got the book'
+    assert book_one.status == 'borrowed'
+    assert book_two.status == 'borrowed'
+    assert book_one.get_book() == None
+    assert book_two.get_book() == None
+    logger.info('Test test_get_book passed')
+
+
+def test_return_book(book_one, book_two):
+    book_one.status = 'borrowed'
+    assert book_one.return_book() == f'{book_one.title} by {book_one.author} is available now'
+    assert book_two.return_book() == None
+    logger.info('Test test_return_book passed')
+
+
+def test_reserving_book(book_one, reader_one, reader_two):
+    assert reader_one.reserving_book(book_one) == None
+    assert book_one.title in reader_one.booked
+    assert book_one.status == 'booked'
+    assert reader_one.reserving_book(book_one) == 'User has already booked this book'
+    assert reader_two.reserving_book(book_one) == \
+           f'{book_one.title} by {book_one.author} is not available. Choose another book.'
+    logger.info('Test test_reserving_book passed')
+
+
+def test_cancelling_reserve(reader_one, reader_two, book_one):
+    book_one.status = 'booked'
+    reader_one.booked = [book_one.title]
+    assert reader_two.cancelling_reserve(book_one) == 'User did not reserve this book'
+    assert reader_one.cancelling_reserve(book_one) == None
+    assert book_one.title not in reader_one.booked
+    logger.info('Test test_cancelling_reserve passed')
+
+
+def test_getting_book(book_one, book_two, reader_one, reader_two):
+    book_one.status = 'booked'
+    reader_one.booked = [book_one.title]
+    assert reader_one.getting_book(book_one) == None
+    assert book_one.title not in reader_one.booked
+    assert book_one.title in reader_one.borrowed
+    assert book_one.status == 'borrowed'
+    assert reader_two.getting_book(book_one) == \
+           f'{book_one.title} by {book_one.author} is not available. Choose another book.'
+    assert book_two.status == 'available'
+    assert book_two.title not in reader_two.booked
+    assert book_two.title not in reader_two.borrowed
+    assert reader_two.getting_book(book_two) == None
+    assert book_two.title in reader_two.borrowed
+    logger.info('Test test_getting_book passed')
+
+
+def test_returning_book(book_one, book_two, reader_one, reader_two):
+    book_one.status = 'borrowed'
+    reader_one.borrowed = [book_one.title]
+    book_two.status = 'booked'
+    reader_two.booked = [book_two.title]
+    assert reader_one.returning_book(book_one) == None
+    assert book_one.title not in reader_one.borrowed
+    assert reader_one.returning_book(book_two) == 'User did not borrow this book'
+    assert reader_two.returning_book(book_two) == 'User did not borrow this book'
+    logger.info('Test test_returning_book passed')
